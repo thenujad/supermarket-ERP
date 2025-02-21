2d75031b817490425f4656bb3770892431b03f57
thenujad <shihanrandina@gmail.com>
2025-02-21 10:49:12 +0530

    backend

---
+++ erp-backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
